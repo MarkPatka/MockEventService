2680fc5cbcd88cbda7df7854e1ee6b89e21a58dc
Mark Patka <mark.kaz.1998@gmail.com>
2026-01-25 21:45:40 +0300

    Adds EventService Abstraction And Expression Extensions

--- MockEventService.Application/DependencyInjection.cs
+++ MockEventService.Application/DependencyInjection.cs
@@ -6,8 +6,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        // MUST REGISTER AND DOMAIN EVENTS AND CQRS - I HOPE...
         services.AddMediatR(cfg => 
-            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly)); 
 
 
         return services;
---
+++ MockEventService.Application/Services/IEventService.cs
@@ -0,0 +1,16 @@
+﻿using MockEventService.Application.EventManagement.Command.CreateEventCommand;
+using MockEventService.Application.Persistence;
+using MockEventService.Application.Persistence.Specifications;
+using MockEventService.Domain.EventAggregate;
+using MockEventService.Domain.EventAggregate.ValueObjects;
+
+namespace MockEventService.Application.Services;
+
+public interface IEventService
+{
+    public Task<IReadOnlyList<Event>> GetEventsByOrganizerIdAsync(OrganizerId id, int pageNumber, int pageSize, CancellationToken cancellationToken);
+    public Task<bool> CheckEventNotExists(string title, OrganizerId request, CancellationToken cancellationToken);
+    public Task<Event?> GetEventByIdAsync(EventId id, CancellationToken cancellationToken = default);
+    public Task<IReadOnlyList<Event>> GetActiveEventsAsync(CancellationToken cancellationToken = default);
+    public Task<Event> CreateEventAsync(Event @event, CancellationToken cancellationToken);
+}
