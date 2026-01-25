c4c621ed4abe086ee2d6c3e2640bb7387fca2566
Mark Patka <mark.kaz.1998@gmail.com>
2026-01-25 21:44:41 +0300

    Adds DBContext, Repositories, UoW, Specification Components

---
+++ MockEventService.Infrastructure/Persistence/MockEventServiceDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MockEventService.Domain.EventAggregate;
+
+namespace MockEventService.Infrastructure.Persistence;
+
+public class MockEventServiceDbContext(DbContextOptions<MockEventServiceDbContext> options) 
+    : DbContext(options)
+{
+    public DbSet<Event> Events { get; set; } = null!;
+
+}
